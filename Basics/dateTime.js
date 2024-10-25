b7c4cedea47fb216aa33dac252c27eb12791d4ee
Sahil Sengupta <iamsahilsengupta@gmail.com>
2024-10-26 00:54:51 +0530

    basic JS codes part 1

--- Basics/dateTime.js
+++ Basics/dateTime.js
@@ -57,3 +57,48 @@
 //     return false
 // }
 // console.log(lastDigit(23,343112,45632))
+
+// Write a JavaScript program to reverse a given string.
+// function reversestr(str){
+//     return str.split('').reverse().join('')
+// }
+// console.log(reversestr("Sahil Sengupta"))
+
+
+// Write a JavaScript program to count the number of vowels in a given string
+// function countVowels(str){
+//     temp=str.toLowerCase().split('')
+//     console.log(temp)
+//     let count = 0
+//     for (let cha of temp) {
+//             if(cha=='a' || cha=='e' || cha=='i' 
+//                 || cha=='o' || cha=='u'){
+//                     count+=1
+//                 }
+//     }
+//     return count
+// }
+// console.log(countVowels("Sahil Sengupta"))
+
+
+// Write a JavaScript program to create a new 
+// string without the first and last characters of a given string. 
+// function newstr(str1){
+//     return str1.slice(1,str1.length-1)
+// } 
+// console.log(newstr("SAAS"))
+
+
+// Write a JavaScript program to find the maximum difference 
+// between any two adjacent elements of a given array of integers. 
+const arr=[10, 15, 21,28, 39, 54, 56, 64]
+let diff = 0
+let pos = 0
+for (let index = 0; index < arr.length-1; index++) {
+    if(Math.abs(arr[index]-arr[index+1])>diff){
+        diff=Math.abs(arr[index]-arr[index+1])
+        pos = index
+    }
+    console.log(pos)
+}
+console.log(`The largest diff is between ${pos} and ${pos+1}`)
