5436636dad51d6e8aef99f761b46eefaa8613ade
Sahil Sengupta <iamsahilsengupta@gmail.com>
2024-10-26 00:24:19 +0530

    basic JS codes

---
+++ Basics/dateTime.js
@@ -0,0 +1,59 @@
+// WAP in JS to get current time
+// const d = new Date();
+// console.log(`The time is ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`)
+
+
+// WAP in JS to get area of a triangle with sides 5,6,7
+// let a = 5
+// let b = 6
+// let c = 7
+// let s = (a+b+c)/2
+// let area = Math.sqrt(s*((s-a)+(s-b)+(s-c)))
+// console.log(`The area is ${area}`)
+
+
+// Write a JavaScript program to create another string by adding "Py" in front of a given string. 
+// If the given string begins with "Py" return the original string. 
+// function addPy(str){
+//     if(str.slice(0,2)=="Py"){
+//         return str
+//     } 
+//     else {
+//         return "Py"+str
+// }
+// }
+// console.log(addPy("P1ython"))
+
+
+// Write a JavaScript program to remove a character 
+// at the specified position in a given string and return the modified string.  
+// function trimmedstr(str, n){
+//     return str.slice(0,n)+str.slice(n+1,str.length)
+// }
+// console.log(trimmedstr("0123456789",9))
+
+
+
+// Write a JavaScript program to check whether a given positive number is a multiple of 3 or 7.  
+// function multiple(n){
+//     if(n%3==0 || n%7==0) return true
+//     return false
+// }
+// console.log(multiple(25))
+
+
+
+// Write a JavaScript program to find the closest value to 100 from two numerical values.
+// let closest = (a,b) => {
+//     if(Math.abs(100-a)<Math.abs(100-b)) return a
+//     return b
+// }
+// console.log(closest(10, 1111))
+
+
+// Write a JavaScript program that checks whether the last digit of three positive integers is the same.
+// let lastDigit=(a,b,c)=>{
+//     if(a%10==b%10 && b%10==c%10) return true
+//     return false
+// }
+// console.log(lastDigit(23,343112,45632))
